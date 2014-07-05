315e9497357be7c443f2f6ecb0e33a8ef5a1898b
celic <cceli@codequarry.net>
2014-07-05 13:16:16 -0400

    Fixes database and rspec issues

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -1,60 +1,56 @@
 require "spec_helper"
 
 describe Point do
-
-	before do
-
-	end
 	
 	context "with normal input" do
 		it "should accept positive numbers" do
-			Point.create(lat: 90, lon: 180).should be_true
+			expect(Point.create(lat: 90, lon: 180)).to be true
 		end
 
 		it "should accept negative numbers" do
-			Point.create(lat: -90, lon: -180).should be_true
+			expect(Point.create(lat: -90, lon: -180)).to be true
 		end
 	end
 
 	context "with positive numbers" do
 		it "should reject large latitude" do
-			p = Point.create(lat: 91, lon: 0).should be_false
+			expect(Point.create(lat: 91, lon: 0)).to be false
 		end
 
 		it "should reject large longitude" do
-			Point.create(lat: 0, lon: 181).should be_false
+			expect(Point.create(lat: 0, lon: 181)).to be false
 		end
 
 		it "should reject large combinations" do
-			Point.create(lat: 91, lon: 181).should be_false
+			expect(Point.create(lat: 91, lon: 181)).to be false
 		end
 	end
 
 	context "with negative numbers" do
 		it "should reject small latitude" do
-			Point.create(lat: -91, lon: 0).should be_false
+			expect(Point.create(lat: -91, lon: 0)).to be false
 		end
 
 		it "should reject small longitude" do
-			Point.create(lat: 0, lon: -181).should be_false
+			expect(Point.create(lat: 0, lon: -181)).to be false
 		end
 
 		it "should reject small combinations" do
-			Point.create(lat: -91, lon: -181).should be_false
+			expect(Point.create(lat: -91, lon: -181)).to be false
 		end
 	end
 
 	context "with irregular input" do
 		it "should reject non-number latitude" do
-			Point.create(lat: 'a'*20, lon: 0).should be_false
+			expect(Point.create(lat: 'a'*20, lon: 0)).to be false
 		end
 
 		it "should reject non-number longitude" do
-			Point.create(lat: 0, lon: 'a'*20).should be_false
+			expect(Point.create(lat: 0, lon: 'a'*20)).to be false
 		end
 
 		it "should reject non-number combinations" do
-			Point.create(lat: 'a'*100, lon: 'a'*50).should be_false
+			expect(Point.create(lat: 'a'*100, lon: 'a'*50)).to be false
 		end
 	end
 end
