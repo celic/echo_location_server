d0028bf3d0de448ba2043a3e298634eeba79d377
celic <cceli@codequarry.net>
2014-07-06 13:22:08 -0400

    Clean up server sample

--- lib/server.rb
+++ lib/server.rb
@@ -1,25 +1,17 @@
 # Gather input
+require 'yaml'
+require 'active_record'
 
 # Get: Location (two ints), S/D (boolean), radius (int)
 
 # If D, store in database
 
-# Use GoogleMaps API to find all other points in DB, within radius from given point
+# Use haversine to find all other points in DB, within radius from given point
 
-dbconfig = YAML::load(File.open('../db/development-database.yml'))
+dbconfig = YAML::load(File.open('db/development-database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig)
 
 # Create point
-point = Point.create(lat: -40, lon: 120)
+@point = Point.create(lat: -40, lon: 120)
 
-puts Point.lat
-
-
-
-# Database
-# point_id --- point
-
-# Point Class
-# int lat
-# int lon
-# Handle bad input, return error from constructor in that case
+puts @point.lat
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-require "yaml"
-require "point"
+require 'yaml'
+require 'point'
 
 dbconfig = YAML::load(File.open('db/test-database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig)
