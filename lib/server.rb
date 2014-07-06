cfd5bffe10961319c8989eff4bfded92f802c9ea
celic <cceli@codequarry.net>
2014-07-06 14:37:29 -0400

    Begins TCP connection

--- lib/server.rb
+++ lib/server.rb
@@ -7,10 +7,18 @@
 # Use haversine to find all other points in DB, within radius from given point
 
 def initialize_database
-
-
 	dbconfig = YAML::load(File.open('db/development-database.yml'))
 	ActiveRecord::Base.establish_connection(dbconfig)
+end
+
+def establish_connection(port)
+	server = TCPServer.new port
+	loop do
+		Thread.start(server.accept) do |client|
+			client.puts "Hello!"
+			client.close
+		end
+	end
 end
 
 def test
--- run_server.rb
+++ run_server.rb
@@ -1,8 +1,16 @@
 require 'yaml'
 require 'active_record'
+require 'socket'
 require_relative 'lib/server'
 require_relative 'lib/point'
 
+# Establish connection with database
 initialize_database
 
+# Test database connection
 test
+
+# Bind TCP Server to port 80
+establish_connection 8000
+
+# From here all logic is passed to lib/server.rb
