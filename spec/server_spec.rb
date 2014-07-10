f144b2122fb1b34eede7e6333d528c5885c935f4
celic <cceli@codequarry.net>
2014-07-09 22:00:26 -0400

    Begins creation of server tests! Everything should be tested. Always always always.

---
+++ spec/server_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Server do
+
+	before do
+		@server = Server.new(1234)
+	end
+
+	context "the initialize_database method" do
+
+	end
+
+	context "the parse_input method" do
+
+	end
+
+	context "the search_points method" do
+
+	end
+
+	context "the haversine method" do
+
+
+	end
+
+	context "the test method" do
+
+	end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 require 'point'
+require 'server'
 
 dbconfig = YAML::load(File.open('db/test-database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig)
